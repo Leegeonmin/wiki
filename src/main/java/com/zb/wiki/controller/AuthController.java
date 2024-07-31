1ef05efee63afa47ee6e7e4912f722f6d1d8c4ef
Leegeonmin <kmlee97s@gmail.com>
2024-08-01 05:48:37 +0900

    feat : MemberController 이름 변경 -> AuthController 회원가입 API url 변경

--- src/main/java/com/zb/wiki/controller/AuthController.java
+++ src/main/java/com/zb/wiki/controller/AuthController.java
@@ -12,10 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/member")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 @Slf4j
-public class MemberController {
+public class AuthController {
 
   private final MemberService memberService;
 
@@ -25,10 +25,12 @@ public class MemberController {
    * @param request 사용자 아이디, 비밀번호, 이메일
    * @return 회원가입 성공메시지
    */
-  @PostMapping
+  @PostMapping("/signup")
   public ResponseEntity<String> signUp(@RequestBody @Valid SignUp.Request request) {
     log.info("signUp request : {}", request);
     memberService.signUp(request.getUsername(), request.getPassword(), request.getEmail());
     return ResponseEntity.ok().body("Sign up successful");
   }
+
+
 }
