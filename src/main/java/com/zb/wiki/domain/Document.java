cae0bb2b47aa573cc8579207e35d44dc0db7b11c
Leegeonmin <kmlee97s@gmail.com>
2024-08-19 01:09:19 +0900

    feat : Document 엔티티 추가

---
+++ src/main/java/com/zb/wiki/domain/Document.java
@@ -0,0 +1,57 @@
+package com.zb.wiki.domain;
+
+import com.zb.wiki.type.DocumentStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@Builder
+@RequiredArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Document {
+
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Id
+  private Long id;
+
+  @Column(unique = true)
+  @NotNull
+  private String title;
+  @NotNull
+  private String context;
+  @Column(nullable = true)
+  private String tag;
+
+  @NotNull
+  @Enumerated(EnumType.STRING)
+  private DocumentStatus documentStatus;
+
+  @CreatedDate
+  private LocalDateTime createdDate;
+  @LastModifiedDate
+  private LocalDateTime modifiedDate;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member modifiedBy;
+}
