04ae73457005ee3ac9fe72444dce0cd493bad485
Leegeonmin <kmlee97s@gmail.com>
2024-08-20 04:03:26 +0900

    chore : unused import delete

--- src/main/java/com/zb/wiki/config/JwtSecurityConfig.java
+++ src/main/java/com/zb/wiki/config/JwtSecurityConfig.java
@@ -1,8 +1,6 @@
 package com.zb.wiki.config;
 
 import com.zb.wiki.security.JwtAuthenticationFilter;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
